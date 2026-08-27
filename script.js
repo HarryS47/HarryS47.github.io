30126bedce975c206d33787d45ca82018873dfeb
Harneesh Singh <coolninja152@gmail.com>
2026-08-27 18:02:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function clockUpdate(){
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, "0");
-  const mins = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
+function clockUpdate() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const mins = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    document.getElementById("clock").textContent = `${hours}:${mins}:${seconds}`;
 }
 
 clockUpdate();
