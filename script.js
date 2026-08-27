3f9b4bc78b2bec0ae78c27e34e79abbd1d0aa71d
Harneesh Singh <coolninja152@gmail.com>
2026-08-27 17:54:02 -0400

    Update script.js
    makign the clock work, also adding "0" to lead with

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function clockUpdate(){
-  const now = newDate();
-  const hours = now.getHours();
-  const mins = now.getMinutes();
-  const seconds = now.getSeconds();
+  const now = newDate ();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const mins = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
 document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
 }
 
-updateClock();
-setInterval(updateClock, 1000);
+clockUpdate();
+setInterval(clockUpdate, 1000);
